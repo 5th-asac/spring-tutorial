6d870798ecb7643618d91b718adc3f1960aaa163
Aaron Ryu <fbcndah@gmail.com>
2024-06-16 21:27:11 +0900

    실습 7-2. UserController : Controller 통해 View 제공 - Class 단위의 @RequestMapping

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -5,14 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user")
+    @RequestMapping(method = RequestMethod.GET, value = "")
     public String userPage() {
         return "/user/user";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/detail")
+    @RequestMapping(method = RequestMethod.GET, value = "/detail")
     public String detailPage() {
         return "/user/detail";
     }
