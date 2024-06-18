082ba0a15ebb54f3d84c488429d0b0b74a0eb483
Aaron Ryu <fbcndah@gmail.com>
2024-06-18 21:27:32 +0900

    실습 9-3. View = ViewTemplate 에 Model 더하는 방법 3) Model

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -4,7 +4,7 @@ import com.example.demo.service.user.User;
 import com.example.demo.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET, value = "")
-    public String userPage(ModelMap model) {
+    public String userPage(Model model) {
         User user = userService.getExampleUser();
 
         model.addAttribute("name", user.getName());
@@ -26,7 +26,7 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/detail")
-    public String detailPage(ModelMap model) {
+    public String detailPage(Model model) {
         User user = userService.getExampleUser();
 
         model.addAttribute("name", user.getName());
