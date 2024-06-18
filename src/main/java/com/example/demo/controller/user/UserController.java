98fee7ee4e23d5324b29fb6ed7ac4636f8aadc0e
Aaron Ryu <fbcndah@gmail.com>
2024-06-18 21:59:55 +0900

    실습 11-2. Spring Bean 사용(주입) 방법 2) 생성자 주입 : Constructor Injection

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.controller.user.dto.UserDetailResponseDto;
 import com.example.demo.controller.user.dto.UserResponseDto;
 import com.example.demo.service.user.User;
 import com.example.demo.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
+    private final UserService userService;
 
     @RequestMapping(method = RequestMethod.GET, value = "")
     public UserResponseDto userData() {
