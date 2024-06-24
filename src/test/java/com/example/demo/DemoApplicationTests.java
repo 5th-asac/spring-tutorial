fccd9a2060c85bcfbb5b6bc325ff0bff4c023b38
Aaron Ryu <fbcndah@gmail.com>
2024-06-24 21:12:15 +0900

    실습 3-2. Docker 로 JAR 실행 시 : Spring Profile(기본값 local) 과 DB 연결 설정 분리

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
+// @PropertySource or @TestPropertySource("classpath:properties.yml")
 class DemoApplicationTests {
 
 	@Test
