3805224856147fb69b1079e5f9532a1afbbe80fb
Aaron Ryu <fbcndah@gmail.com>
2024-06-25 23:44:20 +0900

    실습 5-15. (에러 : 500, BaseResponse 미반환) Controller 계층에서 Exception 에 대한 처리를 ControllerAdvice 에게 책임 위임

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.common.BaseResponse;
-import com.example.demo.common.CustomException;
-import com.example.demo.common.ExceptionType;
 import com.example.demo.controller.dto.UserResponseDto;
 import com.example.demo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -21,17 +17,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public BaseResponse<UserResponseDto> retrieve(@PathVariable Integer id) {
-        try {
-            UserResponseDto response = userService.retrieve(id);
-            return BaseResponse.success(response);
-        } catch (CustomException e) {
-            log.warn(e.getMessage());
-            e.printStackTrace();
-            return BaseResponse.failure(e.getType());
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            e.printStackTrace();
-            return BaseResponse.failure(ExceptionType.INTERNAL_SERVER_ERROR);
-        }
+        UserResponseDto response = userService.retrieve(id);
+        return BaseResponse.success(response);
     }
 }
---
+++ src/main/java/com/example/demo/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller.advice;
+
+import com.example.demo.common.BaseResponse;
+import com.example.demo.common.CustomException;
+import com.example.demo.common.ExceptionType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public BaseResponse custom(CustomException e) {
+        log.warn(e.getMessage());
+        e.printStackTrace();
+        return BaseResponse.failure(e.getType());
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public BaseResponse custom(Exception e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
+        return BaseResponse.failure(ExceptionType.INTERNAL_SERVER_ERROR);
+    }
+}
