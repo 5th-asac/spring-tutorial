c21565e9d6da3b80b598725337889a457a37f049
Aaron Ryu <fbcndah@gmail.com>
2024-06-25 23:11:38 +0900

    실습 5-13. 우리만의 표준 Response 위한 BaseResponse<T> JSON 반환 시 JSON 내 표기 여부 및 방식 결정

--- src/main/java/com/example/demo/common/BaseResponse.java
+++ src/main/java/com/example/demo/common/BaseResponse.java
@@ -1,15 +1,24 @@
 package com.example.demo.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonPropertyOrder({"isSuccess", "body", "error"})
 public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
     private final boolean success;
+    @JsonIgnore
     private final int status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String error;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T body;
 
     public static <T> BaseResponse<T> of(boolean success, int status, String error, T body) {
