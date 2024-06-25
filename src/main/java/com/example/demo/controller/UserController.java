257efaf981badac090d3165ffa4eb5c14fe1427b
Aaron Ryu <fbcndah@gmail.com>
2024-06-25 22:27:58 +0900

    실습 5-8. 다양한 종류의 Exception 처리 필요 : Controller 에선 한개의 Exception 만 처리

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.controller.dto.UserResponseDto;
 import com.example.demo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -21,6 +23,8 @@ public class UserController {
             UserResponseDto response = userService.retrieve(id);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
+            log.warn(e.getMessage());
+            e.printStackTrace();
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/java/com/example/demo/service/user/UserService.java
+++ src/main/java/com/example/demo/service/user/UserService.java
@@ -9,6 +9,8 @@ public class UserService {
     public UserResponseDto retrieve(int id) {
         if (id == 1) {
             return new UserResponseDto(1, "Aaron", 10, "Web Application Developer", "Backend");
+        } else if (id > 10) {
+            throw new RuntimeException("아이디는 10을 넘을 수 없습니다 - id : " + id);
         } else {
             throw new RuntimeException("존재하지 않는 리소스입니다 - id : " + id);
         }
