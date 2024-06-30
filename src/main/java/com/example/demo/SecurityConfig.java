b45c041daf37cc977807043b0032d28450fcfc02
Aaron Ryu <fbcndah@gmail.com>
2024-06-30 19:39:46 +0900

    실습 3-5. Spring Security - UserDetailsManager 통한 로그인 정보(ID/PW/Roles) 인메모리 방식 설정

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -37,4 +42,16 @@ public class SecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/public/*"));
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user").password(passwordEncoder.encode("password")).roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder.encode("password")).roles("USER", "ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
 }
