c1218986386f303c14d821ea8fadb129f9fc4b28
Aaron Ryu <fbcndah@gmail.com>
2024-07-03 21:13:16 +0900

    실습 3-9. Spring Security - 메서드(RequestMapping) 기반 CORS 설정 : (1) 모든 Origin + (2) Preflight 캐싱 30분

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.controller.dto.ProductDto;
 import com.example.demo.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,7 +25,12 @@ public class DashboardController {
         return "/index";
     }
 
-    @Secured("ROLE_ADMIN")
+    @CrossOrigin
+    /**
+     * - All origins are allowed.
+     * - The HTTP methods allowed are those specified in the @RequestMapping annotation (GET, for this example).
+     * - The time that the preflight response is cached (maxAge) is 30 minutes.
+     */
     @GetMapping("/product")
     @ResponseBody
     public ProductDto product() {
